a25a83db60d9e3bbb26f59e2ccff95d30e9a978d
John McAvey <jmcavey@privacystar.com>
2017-06-12 19:42:24 -0500

    Added applicative

---
+++ RunicPromises/Classes/Promise+Applicative.swift
@@ -0,0 +1,42 @@
+//
+//  Promise+Functor.swift
+//  RunicPromises
+//
+//  Created by John McAvey on 6/12/17.
+//
+
+import Foundation
+import Runes
+import PromiseKit
+
+public func <*> <T, Q>(_ a: Promise<T>, _ f: Promise<(T)->Q>) -> Promise<Q> {
+    return a.apply( f )
+}
+
+public func <* <T>(_ keep: Promise<T>, _ discard: AnyPromise) -> Promise<T> {
+    return keep.sequence( discard )
+}
+
+public func *> <T>(_ discard: AnyPromise, _ keep: Promise<T>) -> Promise<T> {
+    return keep <* discard
+}
+
+public func pure<T>(_ value: T) -> Promise<T> {
+    return Promise(value: value)
+}
+
+extension Promise {
+    public func apply<Q>(_ f: Promise<(T)->Q>) -> Promise<Q> {
+        return f.then { (fn: @escaping (T)->Q) -> Promise<Q> in
+            return fn <^> self
+        }
+    }
+
+    public func sequence(_ next: AnyPromise) -> Promise<T> {
+        return self.then { value in
+            next.then { _ in
+                return Promise(value: value)
+            }
+        }
+    }
+}
